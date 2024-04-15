138ceecc11f44523553355f389a050f10a119fba
Bagas Saras <sarasbagas63@gmail.com>
2024-04-15 18:46:46 +0700

    Create styles app template (color, font)

---
+++ lib/styles.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const darkBlue = Color(0XFF0B6EFE);
+  static const darkGrey = Color(0XFF635C5C);
+}
+
+class TextStyles {
+  static TextStyle title = const TextStyle(
+    fontFamily: 'Outfit',
+    fontWeight: FontWeight.bold,
+    fontSize: 18.0,
+    color: AppColors.darkGrey,
+  );
+
+  static TextStyle body = const TextStyle(
+    fontFamily: 'Outfit',
+    fontWeight: FontWeight.normal,
+    fontSize: 16.0,
+    color: AppColors.darkGrey,
+  );
+}
